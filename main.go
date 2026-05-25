55a258bfb68c55dcd14a3f5b2c9f3eba13017c0c
dariodesalvo <dariodesalvo@gmail.com>
2026-05-25 14:57:57 -0300

    add custom Handlers

--- main.go
+++ main.go
@@ -10,6 +10,18 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	fileServerHandler := http.FileServer(http.Dir("."))
+
+	mux.Handle("/app/", http.StripPrefix("/app/", fileServerHandler))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		mensaje := "OK"
+		datosEnBytes := []byte(mensaje)
+		w.Write(datosEnBytes)
+	})
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
