77865edd9f3c241e490a04d4e40cc89c34ac58f2
dariodesalvo <dariodesalvo@gmail.com>
2026-05-26 18:54:08 -0300

    add Stateful Handlers

--- main.go
+++ main.go
@@ -4,15 +4,40 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+}
+
 func main() {
+
+	apiCfg := &apiConfig{}
 
 	mux := http.NewServeMux()
 
 	fileServerHandler := http.FileServer(http.Dir("."))
 
-	mux.Handle("/app/", http.StripPrefix("/app/", fileServerHandler))
+	strippedHandler := http.StripPrefix("/app/", fileServerHandler)
+
+	metricsHandler := apiCfg.middlewareMetricsInc(strippedHandler)
+
+	mux.Handle("/app/", metricsHandler)
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -21,6 +46,17 @@ func main() {
 		datosEnBytes := []byte(mensaje)
 		w.Write(datosEnBytes)
 	})
+
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		hits := apiCfg.fileserverHits.Load()
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		mensaje := fmt.Sprintf("Hits: %d", hits)
+		datosEnBytes := []byte(mensaje)
+		w.Write(datosEnBytes)
+	})
+
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	server := &http.Server{
 		Addr:    ":8080",
