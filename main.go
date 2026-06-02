01d3421fe688f9d4c23a298a39268f59c41e55cc
dariodesalvo <dariodesalvo@gmail.com>
2026-06-02 16:34:15 -0300

    working in storage

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"slices"
+	"strings"
 	"sync/atomic"
 )
 
@@ -25,6 +28,29 @@ func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+
+	type body struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(w, code, body{Error: msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+
+	dat, err := json.Marshal(payload)
+
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
 func main() {
 
 	apiCfg := &apiConfig{}
@@ -39,7 +65,7 @@ func main() {
 
 	mux.Handle("/app/", metricsHandler)
 
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET  /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		mensaje := "OK"
@@ -47,16 +73,57 @@ func main() {
 		w.Write(datosEnBytes)
 	})
 
-	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("POST  /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
+
+		bad_words := []string{"kerfuffle", "sharbert", "fornax"}
+
+		type returnVals struct {
+			Cleaned_body string `json:"cleaned_body"`
+		}
+
+		type parameters struct {
+			Body string `json:"body"`
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		params := parameters{}
+		err := decoder.Decode(&params)
+		if err != nil {
+			log.Printf("Error decoding parameters: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+
+		if len(params.Body) > 140 {
+			respondWithError(w, 400, "Something went wrong")
+		} else {
+			words := strings.Split(params.Body, " ")
+			respuestas := []string{}
+			for _, word := range words {
+				if slices.Contains(bad_words, strings.ToLower(word)) {
+					word = "****"
+				}
+				respuestas = append(respuestas, word)
+			}
+			resultado := strings.Join(respuestas, " ")
+			respBody := returnVals{
+				Cleaned_body: resultado,
+			}
+
+			respondWithJSON(w, 200, respBody)
+		}
+	})
+
+	mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
 		hits := apiCfg.fileserverHits.Load()
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
-		mensaje := fmt.Sprintf("Hits: %d", hits)
+		mensaje := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", hits)
 		datosEnBytes := []byte(mensaje)
 		w.Write(datosEnBytes)
 	})
 
-	mux.HandleFunc("/reset", apiCfg.handlerReset)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
 	server := &http.Server{
 		Addr:    ":8080",
